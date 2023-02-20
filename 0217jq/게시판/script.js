b778f50c205ff9445cf15fd7d55eeb99eb137cb4
David Byun <byundavid@naver.com>
2023-02-20 16:46:35 +0900

    0220 Jquery 애니메이션 및 효과

--- 0217jq/게시판/script.js
+++ 0217jq/게시판/script.js
@@ -4,13 +4,21 @@ $(function () {
     'background-color': 'green',
     border: '1px solid black',
   });
-  $('.num').text('number');
 });
 
 $('.btn').on('click', () => {
   $('.number li:nth-last-child(1)').css('background-color', 'blue');
   $('ul').toggle();
   $('.btn').html(`<span>${new Date()}</span>`);
+});
+
+$('.top').on('mouseover', (e) => {
+  let madeHtml = '<h3>게시판 놀이</h3>';
+  $('.top').after(madeHtml);
+});
+
+$('.top').on('mouseout', (e) => {
+  $('h3').remove();
 });
 
 $('btn').on({
@@ -24,4 +32,65 @@ $('btn').on({
       background: '#ff2',
     });
   },
+});
+
+$('.title').hover(
+  () => {
+    $('.title').css({
+      'text-decoration': 'underline',
+    });
+  },
+  () => {
+    $('.title').css({
+      'text-decoration': 'none',
+    });
+  }
+);
+
+$('.prev').hover(
+  () => {
+    $('.prev').css({
+      'background-color': '#ff2',
+    });
+  },
+  () => {
+    $('.prev').css({
+      'background-color': '#fff',
+    });
+  }
+);
+
+$(function () {
+  $('.card-header').delegate('.on', 'mouseover focus', (e) => {
+    $('.head').css({
+      'background-color': '#ff2',
+      'text-decoration': 'underline',
+    });
+  });
+  $('.head').addClass('on');
+});
+
+$(document).on('keydown', keyEvent);
+function keyEvent(e) {
+  switch (e.keyCode) {
+    case 37:
+      alert('left');
+      break;
+    case 38:
+      alert('top');
+      break;
+    case 39:
+      alert('right');
+      break;
+    case 40:
+      alert('bottom');
+      break;
+  }
+}
+
+$(document).on('scroll', (e) => {
+  let sc_top = $(this).scrollTop();
+  $('.page').html(
+    `<h2>페이지 스크롤하면 나와요! 맨 위로부터 거리 : ${sc_top}px</h2>`
+  );
 });
